306b791ce779f18aa16c451aa9c4534ee8ae0f37
cdelira <carlamdelira@gmail.com>
2017-06-21 13:20:41 -0700

    Initial commit for website source

---
+++ MVCHELPLAB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHELPLAB.Startup))]
+namespace MVCHELPLAB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
