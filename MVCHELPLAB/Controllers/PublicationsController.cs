d67893cf632c51a254817518436712a9f777f46e
cdelira <carlamdelira@gmail.com>
2017-07-25 11:28:37 -0700

    Adjusted filters to be based on projects rather than authors

--- MVCHELPLAB/Controllers/PublicationsController.cs
+++ MVCHELPLAB/Controllers/PublicationsController.cs
@@ -13,12 +13,6 @@ namespace MVCHELPLAB.Controllers
         //TO DO: Clean up publication code to generate script each separately without hard coding it. To lessen the number of lines of code....
         public ActionResult Index()
         {
-            string[] divAuthorNames = new string[11] { "carter-publistdiv", "hundhausen-publistdiv", "agrawal-publistdiv","pagrawal-publistdiv","brown-publistdiv","farley-publistdiv","nevins-publistdiv","stefik-publistdiv","trent-publistdiv","vatrapu-publistdiv","wingstrom-publistdiv" };
-            string[] tagAuthorNames = new string[11] { "adam-carter", "chris-hundhausen", "anu-agrawal", "pawan-agrawal", "jonathan-brown", "sean-farley", "cole-nevins", "andreas-stefik", "stephen-trent", "ravikiran-vatrapu", "josh-wingstrom" };
-
-            ViewBag.divs = divAuthorNames;
-            ViewBag.tags = tagAuthorNames; 
-
 
             return View();
         }
